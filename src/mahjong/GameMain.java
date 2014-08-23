9a674ba9b70ff7253a95a47a6c027db3b7931f8d
pipikaqiu <pipikaqiu@aliyun.com>
2014-08-23 23:01:06 +0100

    Test after updating

--- src/mahjong/GameMain.java
+++ src/mahjong/GameMain.java
@@ -12,27 +12,27 @@ import mahjong.tiles.*;
  */
 public class GameMain {
 
-    public static void main(String[] args) {
-        Game game = new Game(new JPTileGenerator(), new JPGameJudger(), new StdLogger());
-        String[] names = {"a", "b", "c", "d"};
+  public static void main(String[] args) {
+    Game game = new Game(new JPTileGenerator(), new JPGameJudger(), new StdLogger());
+    String[] names = {"a", "b", "c", "d"};
 
-        for (String name : names) {
-            game.addPlayer(new Player(new DefaultAI(), name));
-        }
-
-        game.init();
-
-        while (!game.finish()) {
-            Tile t = game.currentPlayer().drop();
-            game.dropPoolAdd(t);
-
-            boolean picked = game.checkKong() && game.checkPung() && game.checkChow();
-
-            if (!picked) {
-                game.nextRound();
-                game.currentPlayer().pick(game.getOneTile());
-            }
-        }
+    for (String name : names) {
+      game.addPlayer(new Player(new DefaultAI(), name));
     }
+
+    game.init();
+
+    while (!game.finish()) {
+      Tile t = game.currentPlayer().drop();
+      game.dropPoolAdd(t);
+
+      boolean picked = game.checkKong() && game.checkPung() && game.checkChow();
+
+      if (!picked) {
+        game.nextRound();
+        game.currentPlayer().pick(game.getOneTile());
+      }
+    }
+  }
 
 }
