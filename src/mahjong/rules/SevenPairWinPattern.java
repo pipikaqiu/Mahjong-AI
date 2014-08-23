2321f7e49040a76e0f3fe1afb33f118f1f64d18f
pipikaqiu <pipikaqiu@aliyun.com>
2014-08-23 22:09:00 +0100

    Test commit

--- src/mahjong/rules/SevenPairWinPattern.java
+++ src/mahjong/rules/SevenPairWinPattern.java
@@ -4,20 +4,19 @@ import java.util.List;
 import mahjong.tiles.Tile;
 
 /**
- *
+ * 
  * @author jeky
  */
 public class SevenPairWinPattern implements WinPattern {
 
-    @Override
-    public boolean match(List<Tile> tiles) {
-        for (int i = 0; i < 7; i++) {
-            if (!tiles.get(i * 2).equals(tiles.get(i * 2 + 1))) {
-                return false;
-            }
-        }
-
-        return true;
+  @Override
+  public boolean match(List<Tile> tiles) {
+    for (int i = 0; i < 7; i++) {
+      if (!tiles.get(i * 2).equals(tiles.get(i * 2 + 1))) {
+        return false;
+      }
     }
 
+    return true;
+  }
 }
